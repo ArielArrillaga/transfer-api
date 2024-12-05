71f6bf838dc7e9e770e5c93c4b9fad31fa5e064c
Ariel Arrillaga <arielarrillagagm@gmail.com>
2024-12-05 10:00:03 -0300

    Se agrego Swagger

---
+++ src/main/java/com/demo/transfer_api/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.demo.transfer_api.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+	   @Bean
+	    public OpenAPI customOpenAPI() {
+	        return new OpenAPI()
+	                .info(new Info().title("TRANSFER"))
+	                .addSecurityItem(new SecurityRequirement().addList("bearerToken"))
+	                .components(new io.swagger.v3.oas.models.Components()
+	                        .addSecuritySchemes("bearerToken", new SecurityScheme()
+	                                .type(SecurityScheme.Type.HTTP)
+	                                .scheme("bearer")
+	                                .bearerFormat("JWT")));
+	    }
+	 @Bean
+	    public GroupedOpenApi publicApi() {
+	        return GroupedOpenApi.builder()
+	                .group("public")
+	                .pathsToMatch("/**")
+	                .build();
+	    }
+}
