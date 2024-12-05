f58308fa284d1d9f6c1439367c47d58b10f7cadf
Ariel Arrillaga <arielarrillagagm@gmail.com>
2024-12-05 09:12:22 -0300

    Base spring initializr

---
+++ src/main/java/com/demo/transfer_api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.demo.transfer_api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TransferApiApplication.class);
+	}
+
+}
