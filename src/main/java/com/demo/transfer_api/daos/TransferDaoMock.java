a7b818609df72323ee1759166b10a57fe9a995e1
Ariel Arrillaga <arielarrillagagm@gmail.com>
2024-12-11 10:40:21 -0300

    Se externalizo url de mock

--- src/main/java/com/demo/transfer_api/daos/TransferDaoMock.java
+++ src/main/java/com/demo/transfer_api/daos/TransferDaoMock.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.XML;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import com.banelco.transferencias.ws.AgendaCBUDTO;
 import com.demo.transfer_api.utils.CallHttp;
@@ -15,16 +16,17 @@ import com.demo.transfer_api.utils.Mapper;
 
 @Repository
 public class TransferDaoMock implements ITransferDao{
+	@Value("mockSoapUrl")
+	String urlMock;
 
 	@Override
 	public List<AgendaCBUDTO> getRecipients(String numDoc, String typeDoc) {
-		//JAXBContext context;
 		
 		//llamado a la api mock
 		String responseMock = "";
 		try {
-			//Deberia armar el request para el servicio, pero, por la forma en que esta creado el mock, no necesito usar los parametros de entrada.
-			responseMock = CallHttp.llamadoHttpPost(new URL("http://localhost:3003/servicios/transferenciasV2"));
+			//Deberia armar el request para el servicio, pero por la forma en que esta creado el mock, no necesito usar los parametros de entrada.
+			responseMock = CallHttp.llamadoHttpPost(new URL(urlMock));
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -43,7 +45,6 @@ public class TransferDaoMock implements ITransferDao{
 			JSONArray JSONArrayAgendaCbu = json.getJSONArray("agendaCBU");
 
 			return Mapper.mapArrayToAgenda(JSONArrayAgendaCbu);
-			
 		
 		} catch (Exception e) { //para cualquier caso que no sea OK
 			// agregar exceptionHandlers y logs
